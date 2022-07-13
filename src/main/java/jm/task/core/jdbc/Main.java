b468027cb8bf0b3fb3fa028eea1dc7cab63d4a98
RoKenshi <rouronikenshi@gmail.com>
2022-07-13 11:23:46 +0300

    my quest

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
-        System.out.println("hello world!!!");
-        System.out.println("hello world!!!");
+        Util.getUtil().getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+
+        userDao.saveUser("Lyalya", "Kata", (byte) 5);
+        userDao.saveUser("Po", "Kata", (byte) 6);
+        userDao.saveUser("Tinky", "Kata", (byte) 8);
+        userDao.saveUser("Winky", "kata", (byte) 7);
+
+        userDao.removeUserById(1);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,41 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mytest";
+    private static final String NAME = "root";
+    private static final String PASS = "root";
+    private static Util util;
+
+
+    public Util () {}
+
+
+    public static Util getUtil() {
+        if (util == null) {
+            util = new Util();
+        }
+        return util;
+    }
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, NAME, PASS);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Statement getStatement() {
+        try {
+            return getConnection().createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
